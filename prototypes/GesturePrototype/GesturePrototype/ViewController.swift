b7b96fb951978f95926178c863dc4e37f15a3c7a
Dorus Janssens <dorus.janssens@gmail.com>
2018-04-09 14:09:16 +0200

    Add prototypes to resolve upload problem

---
+++ prototypes/GesturePrototype/GesturePrototype/ViewController.swift
@@ -0,0 +1,228 @@
+//
+//  ViewController.swift
+//  GesturePrototype
+//
+//  Created by Dorus Janssens on 08.04.18.
+//  Copyright © 2018 HSLU. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+    
+    var selectedObject: SCNNode?
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Add functionality
+        addTapGestureToSceneView()
+        addDoubleTapGestureToSceneView()
+        addLongTapGestureToSceneView()
+        addSwipeGestureToSceneView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin]
+        
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+        
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    
+    
+    // Show example box
+    func previewBox() {
+        let box = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0.0025)
+        
+        let boxNode = SCNNode()
+        boxNode.geometry = box
+        boxNode.position = SCNVector3Make(0, 0, -0.2)
+        sceneView.pointOfView?.addChildNode(boxNode)
+    }
+    
+    // Add box to the scene
+    func addBox(x: Float = 0, y: Float = 0, z: Float = 0) {
+        let box = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0.0025)
+        
+        let boxNode = SCNNode()
+        boxNode.geometry = box
+        boxNode.position = SCNVector3(x, y, z)
+        
+        boxNode.castsShadow = true
+        
+        box.firstMaterial?.diffuse.contents  = UIColor(red: 182.0 / 255.0, green: 155.0 / 255.0, blue: 76.0 / 255.0, alpha: 1)
+        
+        sceneView.scene.rootNode.addChildNode(boxNode)
+    }
+    
+    
+    // Tap gestures
+    func addTapGestureToSceneView() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap(withGestureRecognizer:)))
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    func addDoubleTapGestureToSceneView() {
+        let doubleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didDoubleTap(withGestureRecognizer:)))
+        doubleTapGestureRecognizer.numberOfTapsRequired = 2
+        sceneView.addGestureRecognizer(doubleTapGestureRecognizer)
+    }
+    
+    func addLongTapGestureToSceneView() {
+        let longTapGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.didLongPress(withGestureRecognizer:)))
+        sceneView.addGestureRecognizer(longTapGestureRecognizer)
+    }
+    
+    func addSwipeGestureToSceneView() {
+        let swipeRightGesture = UISwipeGestureRecognizer(target: self, action: #selector(didSwipe(_:)))
+        swipeRightGesture.direction = .right
+        
+        let swipeLeftGesture = UISwipeGestureRecognizer(target: self, action: #selector(didSwipe(_:)))
+        swipeLeftGesture.direction = .left
+        
+        let swipeUpGesture = UISwipeGestureRecognizer(target: self, action: #selector(didSwipe(_:)))
+        swipeUpGesture.direction = .up
+        
+        let swipeDownGesture = UISwipeGestureRecognizer(target: self, action: #selector(didSwipe(_:)))
+        swipeDownGesture.direction = .down
+        
+        sceneView.addGestureRecognizer(swipeLeftGesture)
+        sceneView.addGestureRecognizer(swipeRightGesture)
+        sceneView.addGestureRecognizer(swipeUpGesture)
+        sceneView.addGestureRecognizer(swipeDownGesture)
+    }
+    
+    @objc
+    func didTap(withGestureRecognizer recognizer: UIGestureRecognizer) {
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestOptions: [SCNHitTestOption: Any] = [.boundingBoxOnly: true]
+        let hitTestResults = sceneView.hitTest(tapLocation, options: hitTestOptions)
+
+        guard let node = hitTestResults.first?.node else {return}
+        
+        updateSelectedObject(node: node)
+    }
+    
+    func updateSelectedObject(node: SCNNode) {
+        // Set selected color
+        SCNTransaction.begin()
+        SCNTransaction.animationDuration = 0.5
+        selectedObject?.geometry?.firstMaterial?.diffuse.contents = UIColor(red: 182.0 / 255.0, green: 155.0 / 255.0, blue: 76.0 / 255.0, alpha: 1)
+        SCNTransaction.commit()
+        
+        // Set new selected node
+        selectedObject = node
+        
+        // Set selected color
+        SCNTransaction.begin()
+        SCNTransaction.animationDuration = 0.5
+        selectedObject?.geometry?.firstMaterial?.diffuse.contents = UIColor(red: 130.0 / 255.0, green: 82.0 / 255.0, blue: 1.0 / 255.0, alpha: 1)
+        SCNTransaction.commit()
+    }
+    
+    @objc
+    func didDoubleTap(withGestureRecognizer recognizer: UIGestureRecognizer) {
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation)
+        
+        guard let node = hitTestResults.first?.node else {
+            let hitTestResultWithFeaturePoints = sceneView.hitTest(tapLocation, types: .estimatedHorizontalPlane)
+            if let hitTestResultWithFeaturePoints = hitTestResultWithFeaturePoints.first {
+                let translation = hitTestResultWithFeaturePoints.worldTransform.translation
+                addBox(x: translation.x, y: translation.y, z: translation.z)
+            }
+            return
+        }
+    }
+    
+    @objc
+    func didLongPress(withGestureRecognizer recognizer: UILongPressGestureRecognizer) {
+        let longPressLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(longPressLocation)
+        
+        guard let node = hitTestResults.first?.node else { return }
+        node.removeFromParentNode()
+    }
+    
+    @objc
+    func didSwipe(_ gesture: UISwipeGestureRecognizer) {
+        let currentAngle = sceneView.session.currentFrame?.camera.eulerAngles.y
+        
+        if gesture.direction == .right {
+            let action = SCNAction.rotateBy(x: 0, y: CGFloat(Double.pi/2), z: 0, duration: 0.5)
+            selectedObject?.runAction(action, forKey: "rotate-right")
+        }
+        else if gesture.direction == .left {
+            let action = SCNAction.rotateBy(x: 0, y: CGFloat(-(Double.pi/2)), z: 0, duration: 0.5)
+            selectedObject?.runAction(action, forKey: "rotate-left")
+        }
+        else if gesture.direction == .up {
+            if (currentAngle! > Float(0.785) && currentAngle! < Float(2.356)) {
+                let action = SCNAction.rotateBy(x: 0, y: 0, z: CGFloat(Double.pi/2), duration: 0.5)
+                selectedObject?.runAction(action, forKey: "rotate-up")
+            }
+            else if (currentAngle! > Float(2.356) || currentAngle! < Float(-2.356)) {
+                let action = SCNAction.rotateBy(x: CGFloat(Double.pi/2), y: 0, z: 0, duration: 0.5)
+                selectedObject?.runAction(action, forKey: "rotate-up")
+                
+            }
+            else if (currentAngle! > Float(-2.356) && currentAngle! < Float(-0.785)) {
+                let action = SCNAction.rotateBy(x: 0, y: 0, z: CGFloat(-(Double.pi/2)), duration: 0.5)
+                selectedObject?.runAction(action, forKey: "rotate-up")
+                
+            }
+            else if (currentAngle! > Float(-0.785) && currentAngle! < Float(0.785)) {
+                let action = SCNAction.rotateBy(x: CGFloat(-(Double.pi/2)), y: 0, z: 0, duration: 0.5)
+                selectedObject?.runAction(action, forKey: "rotate-up")
+            }
+        }
+        else if gesture.direction == .down {
+            if (currentAngle! > Float(0.785) && currentAngle! < Float(2.356)) {
+                let action = SCNAction.rotateBy(x: 0, y: 0, z: CGFloat(-(Double.pi/2)), duration: 0.5)
+                selectedObject?.runAction(action, forKey: "rotate-down")
+            }
+            else if (currentAngle! > Float(2.356) || currentAngle! < Float(-2.356)) {
+                let action = SCNAction.rotateBy(x: CGFloat(-(Double.pi/2)), y: 0, z: 0, duration: 0.5)
+                selectedObject?.runAction(action, forKey: "rotate-down")
+                
+            }
+            else if (currentAngle! > Float(-2.356) && currentAngle! < Float(-0.785)) {
+                let action = SCNAction.rotateBy(x: 0, y: 0, z: CGFloat(Double.pi/2), duration: 0.5)
+                selectedObject?.runAction(action, forKey: "rotate-down")
+                
+            }
+            else if (currentAngle! > Float(-0.785) && currentAngle! < Float(0.785)) {
+                let action = SCNAction.rotateBy(x: CGFloat(Double.pi/2), y: 0, z: 0, duration: 0.5)
+                selectedObject?.runAction(action, forKey: "rotate-down")
+            }
+            
+        }
+        selectedObject?.orientation = SCNQuaternion(x: 0.0, y: 0.0, z: 0.0, w: 1.0)
+    }
+    
+}
+
+// Helper function to convert a matrix to float3 -> x,y,z
+extension float4x4 {
+    var translation: float3 {
+        let translation = self.columns.3
+        return float3(translation.x, translation.y, translation.z)
+    }
+}
+
---
+++ prototypes/arkit-1.5/prototype-01/prototype-01/ViewController.swift
@@ -0,0 +1,120 @@
+//
+//  ViewController.swift
+//  prototype-01
+//
+//  Created by Dorus Janssens on 15.03.18.
+//  Copyright © 2018 HSLU. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Add box
+        addBox()
+        addTapGestureToSceneView()
+        
+        // Create a new scene
+        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        //sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func addBox(x: Float = 0, y: Float = 0, z: Float = -0.2) {
+        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        
+        let boxNode = SCNNode()
+        boxNode.geometry = box
+        boxNode.position = SCNVector3(x, y, z)
+        
+        sceneView.scene.rootNode.addChildNode(boxNode)
+    }
+    
+    func addTapGestureToSceneView() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap(withGestureRecognizer:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    @objc func didTap(withGestureRecognizer recognizer: UIGestureRecognizer) {
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation)
+        guard let node = hitTestResults.first?.node else {
+            let hitTestResultsWithFeaturePoints = sceneView.hitTest(tapLocation, types: .featurePoint)
+            if let hitTestResultWithFeaturePoints = hitTestResultsWithFeaturePoints.first {
+                let translation = hitTestResultWithFeaturePoints.worldTransform.translation
+                addBox(x: translation.x, y: translation.y, z: translation.z)
+            }
+            return
+        }
+        node.removeFromParentNode()
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+    
+    extension float4x4 {
+        var translation: float3 {
+            let translation = self.columns.3
+            return float3(translation.x, translation.y, translation.z)
+        }
+    }
+}
