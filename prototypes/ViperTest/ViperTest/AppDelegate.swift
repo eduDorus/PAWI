01bff38c38e95563d18d0622e7928ed282b6a253
Lucas <lucas.schnueriger@me.com>
2018-05-06 22:43:37 +0200

    make renaming in AppDelegate and storyboard

--- prototypes/ViperTest/ViperTest/AppDelegate.swift
+++ prototypes/ViperTest/ViperTest/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let menuView = MenuWireframe.createMenuModule()
+        let menuView = SelectModeWireframe.createSelectModeModule()
 //        let listView = ListWireframe.createListModule(in: .builder)
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = menuView
