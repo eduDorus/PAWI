3cec048b79769206ee68a7a551ff78fc9ff6fabc
Lucas <lucas.schnueriger@me.com>
2018-05-17 18:40:01 +0200

    add menu button and action sheet (w/o functionality yet)

--- app/ARMarbleRun/Modules/ARBuilder/ARBuilderView.swift
+++ app/ARMarbleRun/Modules/ARBuilder/ARBuilderView.swift
@@ -30,6 +30,11 @@ class ARBuilderView : UIViewController, ARBuilderViewProtocol, ARSCNViewDelegate
         presenter?.didPressNext()
     }
     
+    @IBAction func didPressMenu(_ sender: Any) {
+        presenter?.didPressMenuButton()
+        menuAction()
+    }
+    
     override func viewDidLoad() {
         presenter?.viewDidLoad()
     }
@@ -81,6 +86,31 @@ class ARBuilderView : UIViewController, ARBuilderViewProtocol, ARSCNViewDelegate
         } else if state == .runPlacement {
             marbleRun?.toggleConstraints()
         }
+    }
+    
+    @IBAction func menuAction() {
+        // Create the action buttons for the alert.
+        let saveAction = UIAlertAction(title: "Save", style: .default) { (action) in
+                                            // Respond to user selection of the action.
+        }
+        let changeModeAction = UIAlertAction(title: "Change to Build Mode", style: .default) { (action) in
+                                            // Respond to user selection of the action.
+        }
+        let leaveAction = UIAlertAction(title: "Back to Menu", style: .destructive) { (action) in
+                                            // Respond to user selection of the action.
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+                                            // Respond to user selection of the action.
+        }
+        
+        // Create and configure the alert controller.
+        let alert = UIAlertController(title: "Menu", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(saveAction)
+        alert.addAction(changeModeAction)
+        alert.addAction(leaveAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true)
     }
 
 }
