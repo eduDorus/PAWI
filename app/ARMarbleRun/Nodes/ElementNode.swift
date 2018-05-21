65cfbc72270a2eb8055d6a0b555c4320ce37e405
Lucas <lucas.schnueriger@me.com>
2018-05-21 16:11:10 +0200

    improve element node by using geometry first level and introduce proper state changes

--- app/ARMarbleRun/Nodes/ElementNode.swift
+++ app/ARMarbleRun/Nodes/ElementNode.swift
@@ -11,6 +11,7 @@ class ElementNode : SCNNode, ElementProtocol {
     var type = 12
     var location = Triple(0, 0, 0)
     let sidelength: CGFloat = 0.05
+    let normalColor = UIColor(hue: 33, saturation: 0.24, brightness: 0.97, alpha: 1)
     
     init(type: Int, location: Triple<Int, Int, Int>) {
         super.init()
@@ -21,6 +22,19 @@ class ElementNode : SCNNode, ElementProtocol {
     
     func set(state: ElementState) {
         self.state = state
+        switch self.state {
+        case .faded:
+            set(color: UIColor.white)
+            set(transparency: 0.3)
+        case .normal:
+            set(color: normalColor)
+            set(transparency: 1)
+        case .highlighted:
+            set(color: UIColor.red)
+            set(transparency: 0.5)
+        case .hidden:
+            set(transparency: 0)
+        }
     }
     
     func getState() -> ElementState {
@@ -32,6 +46,22 @@ class ElementNode : SCNNode, ElementProtocol {
         updatePosition()
     }
     
+    func set(color: UIColor) {
+        self.geometry?.firstMaterial?.diffuse.contents = color
+        self.geometry?.firstMaterial?.specular.contents = color
+        enumerateChildNodes { (node, _) in
+            node.geometry?.firstMaterial?.diffuse.contents = color
+            node.geometry?.firstMaterial?.specular.contents = color
+        }
+    }
+    
+    func set(transparency: CGFloat) {
+        self.geometry?.firstMaterial?.transparency = transparency
+        enumerateChildNodes { (node, _) in
+            node.geometry?.firstMaterial?.transparency = transparency
+        }
+    }
+    
     func getLocation() -> Triple<Int, Int, Int> {
         return self.location
     }
@@ -39,22 +69,30 @@ class ElementNode : SCNNode, ElementProtocol {
     func remove() {
         // apparently it's not enough for the node to be removed itself, all its child nodes have to be removed as well
         // maybe there is another way, a different way to build these node so to make it work simpler
-        enumerateChildNodes { (node, stop) in
+        enumerateChildNodes { (node, _) in
             node.removeFromParentNode()
         }
         removeFromParentNode()
     }
     
     private func updatePosition() {
-        self.position = SCNVector3(CGFloat(self.location.values.0) * self.sidelength, CGFloat(self.location.values.1) * self.sidelength + (self.sidelength/2), CGFloat(self.location.values.2) * self.sidelength)
+        let (x,y,z) = self.location.values
+        self.position = SCNVector3(
+            CGFloat(x) * self.sidelength - (self.sidelength/2),
+            CGFloat(y) * self.sidelength,
+            CGFloat(z) * self.sidelength + (self.sidelength/2)
+        )
     }
     
     private func setGeometry() {
         if let cubes = SCNScene(named: "art.scnassets/cuboro-set.scn")?.rootNode {
             if let cube = cubes.childNodes[0].childNodes.filter({ $0.name == "instance_\(type)" }).first {
-                cube.position = SCNVector3(-0.025,-0.025,0.025)
-                cube.scale = SCNVector3(x: 0.00127, y: 0.00127, z: 0.00127)
-                addChildNode(cube)
+                self.geometry = cube.geometry
+                self.rotation = cube.rotation
+                self.scale = SCNVector3(x: 0.00127, y: 0.00127, z: 0.00127)
+                for child in cube.childNodes {
+                    addChildNode(child)
+                }
             }
         } else {
             fatalError("cube \(type) has not been found")
