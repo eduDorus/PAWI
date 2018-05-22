30a59609ddcf4d8993f3195cf950ad4cce11142f
Lucas <lucas.schnueriger@me.com>
2018-05-22 15:53:03 +0200

    add about view with informations about the app and project

---
+++ app/ARMarbleRun/AboutView.swift
@@ -0,0 +1,41 @@
+//
+//  AboutView.swift
+//  ARMarbleRun
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class AboutView: UIViewController {
+
+    @IBAction func didPressClose(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func didPressGitHubLink(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://github.com/eduDorus/PAWI")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func didPressWarehouseLink(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://3dwarehouse.sketchup.com/model/186c758680cfa2a522da50b0bf761759")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+}
