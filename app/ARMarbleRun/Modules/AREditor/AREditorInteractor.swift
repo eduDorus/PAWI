af1e13605471bb9276b203b60cc418a32ff2ce75
Lucas <lucas.schnueriger@me.com>
2018-05-25 14:37:49 +0200

    refactor possible positions method into smaller (simpler?) methods

--- app/ARMarbleRun/Modules/AREditor/AREditorInteractor.swift
+++ app/ARMarbleRun/Modules/AREditor/AREditorInteractor.swift
@@ -16,41 +16,48 @@ class AREditorInteractor : AREditorInteractorProtocol {
             return []
         }
     }
-    
+
     func getPossiblePositions() -> Set<Triple<Int, Int, Int>> {
         var result = Set<Triple<Int, Int, Int>>()
-        if marbleRun != nil {
-            for element in marbleRun!.elements {
-                let (x, y, z) = element.location.values
-                if (y == 0) {
-                    if marbleRun!.getElement(at: Triple(x, y, z + 1)) == nil {
-                        result.insert(Triple(x,y,z + 1))
+
+        result.formUnion(possibleBaselevelPositions())
+        result.formUnion(possibleTopLocations())
+
+        return result
+    }
+
+    private func possibleBaselevelPositions() -> Set<Triple<Int, Int, Int>> {
+        var result = Set<Triple<Int, Int, Int>>()
+        if let baseElementsPositions = getAllBaseElements(except: nil) {
+            for pos in baseElementsPositions {
+                let possibleNeighbors = neighborPositions(from: pos)
+                for pn in possibleNeighbors {
+                    if marbleRun!.getElement(at: pn) == nil {
+                        result.insert(pn)
                     }
-                    if marbleRun!.getElement(at: Triple(x, y, z - 1)) == nil {
-                        result.insert(Triple(x,y,z - 1))
-                    }
-                    if marbleRun!.getElement(at: Triple(x + 1, y, z )) == nil {
-                        result.insert(Triple(x + 1,y,z))
-                    }
-                    if marbleRun!.getElement(at: Triple(x - 1, y, z)) == nil {
-                        result.insert(Triple(x - 1,y,z))
-                    }
-                }
-                if marbleRun!.getElement(at: Triple(x, y + 1, z)) == nil {
-                    result.insert(Triple(x,y + 1,z))
                 }
             }
-            return result
         }
         return result
     }
-    
+
+    private func possibleTopLocations() -> Set<Triple<Int, Int, Int>> {
+        var result = Set<Triple<Int, Int, Int>>()
+        for e in marbleRun!.elements {
+            if !hasElementOnTop(of: e.location) {
+                let (x,y,z) = e.location.values
+                result.insert(Triple(x, y+1, z))
+            }
+        }
+        return result
+    }
+
     func buildElement(element: ElementEntity) {
         marbleRun!.elements.append(element)
     }
-    
+
     func removeElement(at location: Triple<Int, Int, Int>) -> Bool {
-        if hasElementOnTop(at: location) {
+        if hasElementOnTop(of: location) {
             return false
         }
         if (location.values.1 == 0 && !checkRemoveElement(at: location)) {
@@ -59,26 +66,26 @@ class AREditorInteractor : AREditorInteractorProtocol {
         marbleRun!.removeElement(at: location)
         return true
     }
-    
-    func hasElementOnTop(at location: Triple<Int, Int, Int>) -> Bool {
-        var elements = marbleRun?.elements
-        for element in elements! {
-            if element.location.values.1 == location.values.1 + 1 {
-                return false
+
+    private func hasElementOnTop(of location: Triple<Int, Int, Int>) -> Bool {
+        for element in marbleRun!.elements {
+            let (x,y,z) = location.values
+            if element.location == Triple(x, y+1, z) {
+                return true
             }
         }
-        return true
+        return false
     }
-    
+
     func checkRemoveElement(at location: Triple<Int, Int, Int>) -> Bool {
         let elementLocations = getAllBaseElements(except: location)
         // make sure there is always at least one cube
         if elementLocations?.count == 0 { return false }
-        
+
         var visited : [Triple<Int, Int, Int>] = []
         var next: [Triple<Int, Int, Int>] = []
         next.append((elementLocations?.first)!)
-        
+
         while next.count != 0 {
             let nextElement = next.first
             let p: [Triple<Int, Int, Int>] = neighborPositions(from: nextElement)
@@ -104,22 +111,22 @@ class AREditorInteractor : AREditorInteractorProtocol {
         return p
     }
 
-    private func getAllBaseElements(except location: Triple<Int, Int, Int>) -> [Triple<Int, Int, Int>]? {
+    private func getAllBaseElements(except location: Triple<Int, Int, Int>?) -> [Triple<Int, Int, Int>]? {
         let elementLocations = marbleRun?.elements.filter({ (e) -> Bool in
-            e.location.values.1 == 0 && e.location != location
-        })
-        .map({ (e) -> Triple<Int, Int, Int> in
-            e.location
-        })
+                e.location.values.1 == 0 && e.location != location
+            })
+            .map({ (e) -> Triple<Int, Int, Int> in
+                e.location
+            })
         return elementLocations
     }
 
     func selectElement(at location: Triple<Int, Int, Int>) {
     }
-    
+
     func rotateElement(to direction: RotationDirection) {
     }
-    
+
     func persist() {
         let dataManager = MarbleRunDataManager.init()
         dataManager.persist(marbleRun!)
